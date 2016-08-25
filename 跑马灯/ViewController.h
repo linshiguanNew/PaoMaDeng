fbb80a2d08dcdaff249d21a1988b6ef6338d8370
linshiguan <1754564162@qq.com>
2016-08-25 15:24:34 +0800

    跑马灯
    第一次添加

---
+++ 跑马灯/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  跑马灯
+//
+//  Created by 耿岩 on 16/4/25.
+//  Copyright © 2016年 耿岩. All rights reserved.
+//
+/*
+ 1.虽然不怎么实用，但是可以了解大概思路，通过改变视图的center来实现
+ 2.确定赶紧定时器一直在执行，太耗性能了
+ */
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
